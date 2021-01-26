6499fd9f33b072460cfc2244df4146a3e2e14163
EanBott <71108032+EanBott@users.noreply.github.com>
2021-01-26 13:21:58 -0600

    New Project

---
+++ MultiUserProject/Intermediate/Build/Win64/UE4Editor/Inc/MultiUserProject/MultiUserProjectPlayerController.generated.h
@@ -0,0 +1,93 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MULTIUSERPROJECT_MultiUserProjectPlayerController_generated_h
+#error "MultiUserProjectPlayerController.generated.h already included, missing '#pragma once' in MultiUserProjectPlayerController.h"
+#endif
+#define MULTIUSERPROJECT_MultiUserProjectPlayerController_generated_h
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_SPARSE_DATA
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_RPC_WRAPPERS
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMultiUserProjectPlayerController(); \
+	friend struct Z_Construct_UClass_AMultiUserProjectPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AMultiUserProjectPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MultiUserProject"), NO_API) \
+	DECLARE_SERIALIZER(AMultiUserProjectPlayerController)
+
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAMultiUserProjectPlayerController(); \
+	friend struct Z_Construct_UClass_AMultiUserProjectPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(AMultiUserProjectPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MultiUserProject"), NO_API) \
+	DECLARE_SERIALIZER(AMultiUserProjectPlayerController)
+
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMultiUserProjectPlayerController(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMultiUserProjectPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMultiUserProjectPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMultiUserProjectPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMultiUserProjectPlayerController(AMultiUserProjectPlayerController&&); \
+	NO_API AMultiUserProjectPlayerController(const AMultiUserProjectPlayerController&); \
+public:
+
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMultiUserProjectPlayerController(AMultiUserProjectPlayerController&&); \
+	NO_API AMultiUserProjectPlayerController(const AMultiUserProjectPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMultiUserProjectPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMultiUserProjectPlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AMultiUserProjectPlayerController)
+
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_PRIVATE_PROPERTY_OFFSET
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_9_PROLOG
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_SPARSE_DATA \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_RPC_WRAPPERS \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_INCLASS \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_SPARSE_DATA \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+	MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MULTIUSERPROJECT_API UClass* StaticClass<class AMultiUserProjectPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MultiUserProject_Source_MultiUserProject_MultiUserProjectPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
