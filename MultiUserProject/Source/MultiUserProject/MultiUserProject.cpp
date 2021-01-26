4f9202cbc08686617f79b15574426373b47bcfcc
EanBott <71108032+EanBott@users.noreply.github.com>
2021-01-26 13:22:54 -0600

    New project

---
+++ MultiUserProject/Source/MultiUserProject/MultiUserProject.cpp
@@ -0,0 +1,9 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "MultiUserProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiUserProject, "MultiUserProject" );
+
+DEFINE_LOG_CATEGORY(LogMultiUserProject)
+ 
